3087c19196c701f6ff1298ccb17b9e879c95a58c
harinagendra02 <harinagendra486@gmail.com>
2026-06-18 15:58:14 -0500

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from apify_client import ApifyClient
 client = ApifyClient(os.getenv("APIFY_TOKEN"))
 
 run_input = {
-    "searchKeywords": "java developer tx gc",
+    "keywords": "java developer tx gc",
     "maximumPosts": 5
 }
 
@@ -12,5 +12,5 @@ run = client.actor("benjarapi/linkedin-post-search").call(
     run_input=run_input
 )
 
-print("Run ID:", run["id"])
-print("Dataset ID:", run["defaultDatasetId"])
+print("Run ID:", run.id)
+print("Dataset ID:", run.default_dataset_id)
