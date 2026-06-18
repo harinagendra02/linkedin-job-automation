67bde8168fc7293da851831932af82087196deae
harinagendra02 <harinagendra486@gmail.com>
2026-06-18 14:23:34 -0500

    Create sheets.py

---
+++ sheets.py
@@ -0,0 +1,19 @@
+import gspread
+from oauth2client.service_account import ServiceAccountCredentials
+
+scope = [
+    "https://spreadsheets.google.com/feeds",
+    "https://www.googleapis.com/auth/drive"
+]
+
+creds = ServiceAccountCredentials.from_json_keyfile_name(
+    "credentials.json",
+    scope
+)
+
+client = gspread.authorize(creds)
+
+sheet = client.open("LinkedIn Job Automation").sheet1
+
+def add_row(data):
+    sheet.append_row(data)
